83783b65e5453201d35ec905559ea9ac77afb554
Maikoncarlos <maikonsaturno@gmail.com>
2025-03-03 17:14:48 -0300

    commit initial

---
+++ src/main/java/com/github/maikoncarlos/webflux_api_reativa/WebfluxApiReativaApplication.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.webflux_api_reativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxApiReativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxApiReativaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/maikoncarlos/webflux_api_reativa/WebfluxApiReativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maikoncarlos.webflux_api_reativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxApiReativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
